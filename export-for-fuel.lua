72d47ce795ce3d041275fc8b969bf98f350326ea
Lirby <83455719+Lirby@users.noreply.github.com>
2025-06-27 10:15:47 +0200

    Add files via upload

---
+++ export-for-fuel.lua
@@ -0,0 +1,40 @@
+local meBridge = peripheral.wrap("left")
+local chest = peripheral.wrap("right")
+
+local items = {
+    { name = "thermal:sulfur", count = 1 },
+    { name = "mekanism:uranium", count = 1 },
+    { name = "mekanism:fluorite", count =1 },
+}
+
+local function isItemAvailable(itemName, amount)
+    local details = meBridge.getItem({ name = itemName })
+    return details and details.amount >= amount
+end
+
+while true do
+    local allAvailable = true
+    
+    for _, item in ipairs(items) do
+        if not isItemAvailable(item.name, item.count) then
+            print("nicht genug vorhanden:" .. item.name .. "Warte auf Nachschub")
+            allAvailable = false
+            break
+        end
+    end
+    
+    if allAvailavle then
+        for _, item in ipairs(items) do
+            local exported = meBridge.exportItemFormSystem(item, chest)
+            if exported then
+                print("Exportiert: " .. item.count .. " von " .. item.name)
+            else
+                print("Fehler beim Export von: " .. item.name)
+            end
+            sleep(0.5)
+        end
+        sleep(5)
+    else 
+        sleep(10)
+    end
+end
