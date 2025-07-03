327769c0b014d390e6de9bb0a34b75b0dfc249c2
Lirby <83455719+Lirby@users.noreply.github.com>
2025-07-03 09:28:25 +0000

    Update Reader.lua

--- Reader.lua
+++ Reader.lua
@@ -1,16 +1,20 @@
 local tanks = {
     left = peripheral.wrap("left"),
-    right = peripheral.wrap("right")
+    right = peripheral.wrap("right"),
 }
 
 function readChemicalTank(reader)
     if not reader then return nil end
     local data = reader.getBlockData()
-    if not data or not data.GasTanks or not data.GasTanks[1] then
-        return nil
+    if not data or not data.GasTanks or not data.GasTanks[0] then
+        return {
+            amount = 0,
+            gasName = "Leer",
+            capacity = 0
+        }
     end
 
-    local gasTank = data.GasTanks[1]
+    local gasTank = data.GasTanks[0]
     local stored = gasTank.stored
 
     local amount = (stored and stored.amount) or 0
@@ -24,17 +28,13 @@ function readChemicalTank(reader)
     }
 end
 
-for side, reader in pairs(tanks) do
+for side, reader in pairs(tanks) do 
     local tankData = readChemicalTank(reader)
-    if tankData then
-        print(string.format(
-            "%s Tank: %s - %d  / %d mB",
-            side,
-            tankData.gasName,
-            tankData.amount,
-            tankData.capacity
-        ))
-    else
-        print(side .. " Tank: Keine Daten gefunden")
-    end
+    print (string.format(
+        "%s Tank: %s - %d / %d mB",
+        side,
+        tankData.gasName,
+        tankData.amount,
+        tankData.capacity
+    ))
 end
