7aeea19417e394c50b245f3b717eab34962758d1
Lirby <83455719+Lirby@users.noreply.github.com>
2025-12-04 16:34:31 +0100

    Update ether_to_pink-slime.js

--- kubejs-lithium/server_scripts/ether_to_pink-slime.js
+++ kubejs-lithium/server_scripts/ether_to_pink-slime.js
@@ -1,29 +1,13 @@
 onEvent('recipes', event => {
-    const cinna = 'thermal:cinnabar'
-    const ether = 'industrialforegoing:ether_gas'
-    const pink = 'industrialforegoing:pink_slime'
+  const cinna = 'thermal:cinnabar'
+  const ether = 'industrialforegoing:ether_gas'
+  const pink = 'industrialforegoing:pink_slime'
 
-    event.custom({
-        type: "thermal:machine_brewer",
-
-        ingredients: [
-            { item: cinna },
-            { item: cinna },
-            { item: cinna },
-            { item: cinna }
-        ],
-
-        input_fluid: {
-            fluid: ether,
-            amount: 500
-        },
-
-        result: {
-            fluid: pink,
-            amount: 1000
-        },
-
-        energy: 12000
-    })
-    .id('Lirby:thermal/ether_to_pinkslime')
-});
+  event.recipes.thermal.brewer(
+    Fluid.of(pink, 1000),
+    [
+      Fluid.of(ether, 500),
+      '4x ' + cinna
+    ]
+  ).energy(12000).id('lirby:thermal/ether_to_pinkslime')
+})
